0ab7a8c4f3362cb393fa2f9547cbd98b5be65ccf
Victor Lee <myleevictor@gmail.com>
2018-03-23 10:57:58 +0800

    passed test on codewars to return most frequent item in array

---
+++ script.js
@@ -0,0 +1,16 @@
+function mostFrequentItemCount(collection) {
+  if (collection.length === 0) return 0;
+
+  let mapObject = {};
+  collection.forEach(element => {
+    if (mapObject[element] === undefined) {
+      mapObject[element] = 1;
+    } else {
+      mapObject[element]++;
+    }
+  });
+
+  const output = Object.keys(mapObject).map(key => mapObject[key]);
+
+  return Math.max(...output);
+}
